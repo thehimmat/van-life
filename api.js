a9b8fcafc6c6aa6fd30adc0efd3e2382c969e06f
Himmat Singh Khalsa <himmatsinghkhalsa@gmail.com>
2025-02-28 12:49:05 -0800

    uncomment naive loginUser api function

--- api.js
+++ api.js
@@ -45,19 +45,19 @@ export async function getHostVans() {
     return vans
 }
 
-// export async function loginUser(creds) {
-//     const res = await fetch("/api/login",
-//         { method: "post", body: JSON.stringify(creds) }
-//     )
-//     const data = await res.json()
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
 
-//     if (!res.ok) {
-//         throw {
-//             message: data.message,
-//             statusText: res.statusText,
-//             status: res.status
-//         }
-//     }
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
 
-//     return data
-// }
+    return data
+}
